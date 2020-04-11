51e5ba89a78e26ab076f8112b671be4ad283fc55
Jitrapon-poonsuk <jitraponpoonsuk@gmail.com>
2020-04-12 00:02:46 +0700

    Updates

---
+++ precache-manifest.b0bafe07e4b4e3684562c8b220632af4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e45a1faea4644eeeed8076b7ca21e7fd",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "568fdac1b63690cc8efd",
+    "url": "/react-todo-app/static/js/2.16724be7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-todo-app/static/js/2.16724be7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "141806e56f61cde109bd",
+    "url": "/react-todo-app/static/js/main.0b39ac83.chunk.js"
+  },
+  {
+    "revision": "e5c725afc87196764c0a",
+    "url": "/react-todo-app/static/js/runtime-main.1f982136.js"
+  }
+]);
